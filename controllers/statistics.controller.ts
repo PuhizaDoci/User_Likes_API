d27440a5db6eb3c230856eb59cda8965bc812108
Puhizë Doçi <puhizad@gmail.com>
2022-05-01 19:24:48 +0200

    Return users sorted from most liked to least liked

--- controllers/statistics.controller.ts
+++ controllers/statistics.controller.ts
@@ -1,21 +1,41 @@
-//const usersLogic = require('../middlewares/users.middleware');
 import express, { Router, Request, Response } from "express";
-import { authenticateToken } from "../middlewares/auth";
 import { getAllUsers } from '../services/users.service';
+import { getUserLikes } from '../services/userLikes.service';
+import CustomResponse from '../models/response';
 
 const router: Router = express.Router();
 
 // most liked
-router.get("/most-liked", authenticateToken, async (req: Request, res: Response) => {
-    const userLikes = await getAllUsers()
-    
-    // TODO "join" and sort
-
+router.get("/most-liked", async (_req: Request, res: Response) => {
+    let customRes: CustomResponse = {
+        success: false,
+        error: ""
+    };
     try {
-        res.send(userLikes);
-    } catch (error) {
-        res.status(500)
-            .send(error);
+        const users = await getAllUsers()
+        let usersLikes = []
+
+        // not my best work ever :/
+        for await (const user of users) {
+            const userLikes = await getUserLikes(user.userid)
+            let likeNumbers = 0
+
+            for await (const {} of userLikes) {
+                likeNumbers += 1
+            }
+            usersLikes.push({user, likeNumbers})
+        }
+
+        usersLikes.sort((a,b) => b.likeNumbers - a.likeNumbers); 
+
+        customRes.success = true
+        customRes.data = usersLikes
+        res.send(customRes);
+    } catch (err) {
+        customRes.success = false
+        const error = err as Error;
+        customRes.error = error.message
+        res.status(500).send(JSON.stringify(customRes));
     }
 });
 
